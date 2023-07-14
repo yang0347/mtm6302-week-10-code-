4799bd33c9598e0a447dc7b801506e098dd10208
Abhay Sharma <abhaysharmawebd@gmail.com>
2023-07-13 22:47:04 -0400

    adds starting files for week 10

---
+++ js/main.js
@@ -0,0 +1,50 @@
+const cats = [
+  {
+    name: "Cat",
+    bio: "Cat is an English word.",
+    thumb: "/images/kitten1-thumb.jpeg",
+    img: "/images/kitten1.jpeg"
+  },
+  {
+    name: "Mao",
+    bio: "Mao is is a Cantonese word.",
+    thumb: "/images/kitten2-thumb.jpeg",
+    img: "/images/kitten2.jpeg"
+  },
+  {
+    name: "Gato",
+    bio: "Gato is a Spanish word",
+    thumb: "/images/kitten3-thumb.jpeg",
+    img: "/images/kitten3.jpeg"
+  },
+  {
+    name: "Billi",
+    bio: "Billi is a Hindi word.",
+    thumb: "/images/kitten4-thumb.jpeg",
+    img: "/images/kitten4.jpeg"
+  },
+  {
+    name: "Chat",
+    bio: "Chat is a French word.",
+    thumb: "/images/kitten5-thumb.jpeg",
+    img: "/images/kitten5.jpeg"
+  },
+  {
+    name: "Kot",
+    bio: "Kot is a Polish word.",
+    thumb: "/images/kitten6-thumb.jpeg",
+    img: "/images/kitten6.jpeg"
+  },
+  {
+    name: "Kit",
+    bio: "Kit is a Ukrainian word.",
+    thumb: "/images/kitten7-thumb.jpeg",
+    img: "/images/kitten7.jpeg"
+  },
+  {
+    name: "Kot",
+    bio: "Kot is a Russian word.",
+    thumb: "/images/kitten8-thumb.jpeg",
+    img: "/images/kitten8.jpeg"
+  }
+]
